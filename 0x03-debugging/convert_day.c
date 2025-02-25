7fe18a2e0537f911fc35c0d7032d3f1113c1592d
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-02-25 12:14:47 +0200

    Create convert_day.c

---
+++ 0x03-debugging/convert_day.c
@@ -0,0 +1,11 @@
+int convert_day(int month, int day) {
+    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int day_of_year = 0;
+
+    for (int i = 1; i < month; i++) {
+        day_of_year += days_in_month[i];
+    }
+    day_of_year += day;
+
+    return day_of_year;
+}
