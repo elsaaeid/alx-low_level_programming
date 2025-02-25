9f0f32fff02a089e8eadadbfb106f1530cff03d1
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-02-25 12:10:15 +0200

    Create 1-positive_or_negative.c

---
+++ 0x03-debugging/1-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Function to check if a number is positive, negative, or zero
+void positive_or_negative(int i) {
+    if (i > 0) {
+        printf("%d is positive\n", i);
+    } else if (i < 0) {
+        printf("%d is negative\n", i);
+    } else {
+        printf("%d is zero\n", i);
+    }
+}
