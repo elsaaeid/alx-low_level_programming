e4ad813a9c191b1676738f129b485c7a6a8cd4e2
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-03-16 20:40:39 +0200

    Update 4-convert_day.c

--- 0x03-debugging/4-convert_day.c
+++ 0x03-debugging/4-convert_day.c
@@ -1,9 +1,9 @@
 /**
- * convert a date
- * (month and day)
- * into the day of the year
+ * convert_day - converts a date (month and day) into the day of the year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of the year
  */
-
 int convert_day(int month, int day) {
     int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int day_of_year = 0;
