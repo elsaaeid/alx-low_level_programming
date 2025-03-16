e2370f1fe6a04e02d0edd5fc6424be954563a2ae
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-03-16 23:56:49 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 void swap_int(int *a, int *b) {
     int temp = *a; // Store the value pointed to by a in a temporary variable
     *a = *b;       // Assign the value pointed to by b to the location pointed to by a
